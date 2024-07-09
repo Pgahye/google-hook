ac63ec7edd1110a436c0a39501b05f9ed107f8e1
Pgahye <rkgp0515@gmail.com>
2024-07-09 10:00:33 +0900

    Update GameDataController.js

--- Controllers/GameDataController.js
+++ Controllers/GameDataController.js
@@ -4,10 +4,10 @@ var TeamInfo = mongoose.model('TeamInfo');
 var GameSchedule = mongoose.model('GameSchedule');
 
 exports.processRequest = function(req, res) {
-    if (req.body.result.action == "schedule") {
+    if (req.body.queryResult.action == "schedule") {
         getTeamSchedule(req,res)
     }
-    else if (req.body.result.action == "tell.about")
+    else if (req.body.queryResult.action == "tell.about")
     {
         getTeamInfo(req,res)
     }
@@ -15,33 +15,18 @@ exports.processRequest = function(req, res) {
 
 function getTeamInfo(req,res)
 {
-    let teamToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.team ? req.body.result.parameters.team : 'Unknown';
-    TeamInfo.findOne({name:teamToSearch},function(err,teamExists)
-    {
-        if (err)
-        {
+      TeamInfo.find().then((data) => {
+        console.log(data);
+        if(data){
+            return res.json(data);
+        }else{
             return res.json({
                 speech: 'Something went wrong!',
                 displayText: 'Something went wrong!',
-                source: 'team info'
+                source: 'game schedule'
             });
         }
-        if (teamExists)
-        {
-            return res.json({
-                speech: teamExists.description,
-                displayText: teamExists.description,
-                source: 'team info'
-            });
-        }
-        else {
-            return res.json({
-                speech: 'Currently I am not having information about this team',
-                displayText: 'Currently I am not having information about this team',
-                source: 'team info'
-            });
-        }
-    });
+    }
 }
 
 
